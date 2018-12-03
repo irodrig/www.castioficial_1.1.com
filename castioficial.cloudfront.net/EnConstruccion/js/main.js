8124a7e8d96cb3132df693eb29a7b74e3eb80a1a
irodrig <corunabussiness@gmail.com>
2018-12-04 00:26:16 +0100

    Cambio de fecha lanzamiento

--- castioficial.cloudfront.net/EnConstruccion/js/main.js
+++ castioficial.cloudfront.net/EnConstruccion/js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "December 14, 2018 18:03:26",
+	date: "December 30, 2018 18:03:26",
 	render: function(data) {
 		var el = $(this.el);
 		el.empty()
